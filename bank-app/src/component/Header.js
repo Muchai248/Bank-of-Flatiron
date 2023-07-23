3f0f6a384971909c2319ac92b7b0707af28483b3
Mark Muchai <muchaimark6@gmail.com>
2023-07-23 22:42:26 +0300

    initial  commit

--- bank-app/src/component/Header.js
+++ bank-app/src/component/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 function Header(){
     return(
         <>
-            <nav style={{textAlign:"center", color:"white", backgroundColor:"purple", fontSize:"44px"}}>Welcome to the React App</nav>
+            <nav style={{textAlign:"center", bottom:"99px",color:"white", backgroundColor:"purple", fontSize:"44px"}}>Welcome to the React App</nav>
         </>
     )
 }
