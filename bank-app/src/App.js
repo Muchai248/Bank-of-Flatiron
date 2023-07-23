b261f704ffee1c97ba49206a6fa577c80cc16a43
Mark Muchai <muchaimark6@gmail.com>
2023-07-23 21:59:35 +0300

    initial commit

--- bank-app/src/App.js
+++ bank-app/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import Search from './component/Search.js';
 
 import Header from "./component/Header";
 
 function App() {
   return (
+    <>
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
        
       </div>
+      <Search/>
       <Header/>
      
     </div>
+    </>
   );
 }
 
