2d52c2e14bcd241c71d16ffcae8ba8618d45a119
Mark Muchai <muchaimark6@gmail.com>
2023-07-23 21:27:32 +0300

    README.MD

---
+++ bank-app/src/component/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+    return(
+        <>
+        <div>
+            <header>The Royal Bank of Flatiron</header>
+        </div>
+        </>
+    )
+
+}
+
+export default Header;
